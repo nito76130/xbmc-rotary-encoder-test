6a1f11572e2ee202998286b86dbc31321fd646f0
nito76130 <jador_labrador@hotmail.com>
2013-08-27 14:28:46 +0200

    Update rotary-encoder.c

--- rotary-encoder.c
+++ rotary-encoder.c
@@ -7,23 +7,22 @@ int main()
 {
 CURL *curl;
 CURLcode res;
-printf("Hello!\n");
+
 wiringPiSetup () ;
 /*using pins 23/24*/
 struct encoder *encoder = setupencoder(4,5);
 long value;
 char buffer[250];
-char buffer2[250];
 while (1)
  {
  updateEncoders();
  long l = encoder->value;
  if(l!=value)
  {
-     printf("value: %d\n", (void *)l);
+     /*  printf("value: %d\n", (void *)l); */
      curl=curl_easy_init();
      sprintf(buffer, "http://127.0.0.1/jsonrpc?request={\"jsonrpc\":\"2.0\",\"method\":\"Application.SetVolume\",\"params\":{\"volume\":%d},\"id\":1}", l);
-     printf ("testing %s", buffer);
+
      curl_easy_setopt(curl, CURLOPT_URL, buffer);
     /* Perform the request, res will get the return code */
      res = curl_easy_perform(curl);
